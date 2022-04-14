c79318578fc647a69ed3f18895d7e90e4227f03e
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-14 12:25:29 +0200

    feat: add cfn-lint job to build

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,6 @@
 const { awscdk } = require('projen');
+const { JobPermission } = require('projen/lib/github/workflows-model');
+const { NodePackageManager } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkTypeScriptApp({
   projenVersion: '0.54.14',
   cdkVersion: '2.16.0',
@@ -16,6 +18,32 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+  scripts: {
+    lint: 'cfn-lint cdk.out/**/*.template.json -i W3005 W2001',
+  },
+});
+
+/**
+ * Add couldformation lint job to build workflow
+ * Note: Not ideal as we would like to incldue the lint step in the
+ * buidl process, however the setup of the linter cannot be added to
+ * the existing workflow nor can the build task be altered.
+ */
+project.buildWorkflow.addPostBuildJob('CloudFormation lint', {
+  runsOn: ['ubuntu-latest'],
+  permissions: {
+    contents: JobPermission.READ,
+  },
+  steps: [
+    {
+      name: 'Setup Cloud Formation Linter with Latest Version',
+      uses: 'scottbrenner/cfn-lint-action@v2',
+    },
+    {
+      name: 'Run cfn-lint',
+      run: 'npx projen lint', // Lint script should be present in .projenrc.js
+    },
+  ],
 });
 
 project.synth();
