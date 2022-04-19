ca81af203401fc72cff468a84195c68c6fd6f471
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-19 13:31:59 +0200

    feat: enable cfn-nag and make synth:silent task show the output

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Stack, StackProps, Aspects } from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
+    console.log('Test logging in cdk synth');
     // define resources here...
   }
 }
@@ -19,5 +21,10 @@ const app = new App();
 
 new MyStack(app, 'cdk-repository-template-dev', { env: devEnv });
 // new MyStack(app, 'cdk-repository-template-prod', { env: prodEnv });
+
+/**
+ * Enable cfn-nag during cdk synth
+ */
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 
 app.synth();
