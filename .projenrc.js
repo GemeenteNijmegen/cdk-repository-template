f1d304d286194cfe6d93d952867cdac62cbf921a
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-26 00:25:27 +0200

    feat: add cfn-diff post build workflow job

--- .projenrc.js
+++ .projenrc.js
@@ -39,10 +39,10 @@ const project = new awscdk.AwsCdkTypeScriptApp({
       uses: 'actions/upload-artifact@v3',
       with: {
         name: 'build-templates',
-        path: 'cdk.out'
+        path: 'cdk.out',
       },
-    }
-  ]
+    },
+  ],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
@@ -56,22 +56,30 @@ const postCompile = project.tasks.tryFind('post-compile');
 const lint = project.tasks.tryFind('lint');
 postCompile.spawn(lint);
 
-
-// project.buildWorkflow.addPostBuildSteps([]); // Contains a bug in the YAML format
-
-project.buildWorkflow.addPostBuildJob('build_target', {
-  permissions: { contents: JobPermission.READ },
+/**
+ * A job to build the base branch and execute a diff on the build cdk.out and base
+ * branch cdk.out. A comment is added to the PR indicating if there are differences
+ * in the CloudFormation templates.
+ */
+const comment = 'between CloudFormation templates on base branch and this branch.';
+project.buildWorkflow.addPostBuildJob('cfn-diff', {
+  permissions: {
+    contents: JobPermission.READ,
+    pullRequests: JobPermission.WRITE,
+  },
   runsOn: ['ubuntu-latest'],
-  //needs: [], // No need to wait for selfmutation 
-  //if: 'true', // Always run independent of sell mutation
   steps: [
+    {
+      name: 'Keep build CloudFormation templates',
+      run: 'mkdir -p ../cdk.out.build && cp dist/* ../cdk.out.build/',
+    },
     {
       name: 'Checkout',
       uses: 'actions/checkout@v2',
       with: {
-        ref: "${{ github.base_ref }}",
-        repository: "${{ github.event.pull_request.head.repo.full_name }}",
-      }
+        ref: '${{ github.base_ref }}',
+        repository: '${{ github.event.pull_request.head.repo.full_name }}',
+      },
     },
     {
       name: 'Setup cfn-lint',
@@ -86,45 +94,25 @@ project.buildWorkflow.addPostBuildJob('build_target', {
       run: 'yarn build',
     },
     {
-      name: 'Save CloudFormation templates',
-      uses: 'actions/upload-artifact@v3',
-      with: {
-        name: 'base-branch-templates',
-        path: 'cdk.out'
+      name: 'Prepare CloudFormation template directories',
+      run: 'mv ../cdk.out.build cdk.out.build && mv cdk.out cdk.out.base',
+    },
+    {
+      name: 'CloudFormation diff', // TODO: use cdk diff here.
+      run: [
+        'result="$(diff -rq cdk.out.build cdk.out.base || true)"',
+        'echo "$result"',
+        '[ -z "$result" ] && msg="No differences" || msg="Differences"',
+        'echo "Creating a comment on the PR..."',
+        `gh pr comment $PR --body "$(echo $msg) ${comment} \n <details><pre>$(echo "$result")</pre></details>" -R $GITHUB_REPOSITORY`,
+      ].join('; '),
+      env: {
+        GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
+        GITHUB_REPOSITORY: '${{ github.repository }}',
+        PR: '${{ github.event.pull_request.number }}',
       },
     },
-  ]
+  ],
 });
-
-project.buildWorkflow.addPostBuildJob('cfn-diff', {
-  permissions: { contents: JobPermission.READ },
-  runsOn: ['ubuntu-latest'],
-  needs: ['build', 'build_target'],
-  steps: [
-    {
-      name: 'Download base branch CloudFomration templates',
-      uses: 'actions/download-artifact@v3',
-      with: {
-        name: 'base-branch-templates',
-        path: 'cdk.out.base'
-      },
-    },
-    {
-      name: 'Download build CloudFomration templates',
-      uses: 'actions/download-artifact@v3',
-      with: {
-        name: 'build-templates',
-        path: 'cdk.out.build'
-      },
-    },
-    {
-      name: 'Diff',
-      run: 'diff -r -q cdk.out.build cdk.out.base' // Maybe use cdk diff here?
-    },
-  ]
-})
-
-
-
 
 project.synth();
