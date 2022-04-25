50edb9758f789d554e0cb8d848a25a259e774528
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-25 13:20:20 +0200

    feat: diff in cloudformation on PR

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,5 @@
 const { awscdk } = require('projen');
+const { JobPermission } = require('projen/lib/github/workflows-model');
 
 const project = new awscdk.AwsCdkTypeScriptApp({
   projenVersion: '0.54.34',
@@ -32,6 +33,16 @@ const project = new awscdk.AwsCdkTypeScriptApp({
       uses: 'scottbrenner/cfn-lint-action@v2',
     },
   ],
+  postBuildSteps: [
+    {
+      name: 'Save CloudFormation templates',
+      uses: 'actions/upload-artifact@v3',
+      with: {
+        name: 'build-templates',
+        path: 'cdk.out'
+      },
+    }
+  ]
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
@@ -44,5 +55,76 @@ const project = new awscdk.AwsCdkTypeScriptApp({
 const postCompile = project.tasks.tryFind('post-compile');
 const lint = project.tasks.tryFind('lint');
 postCompile.spawn(lint);
+
+
+// project.buildWorkflow.addPostBuildSteps([]); // Contains a bug in the YAML format
+
+project.buildWorkflow.addPostBuildJob('build_target', {
+  permissions: { contents: JobPermission.READ },
+  runsOn: ['ubuntu-latest'],
+  //needs: [], // No need to wait for selfmutation 
+  //if: 'true', // Always run independent of sell mutation
+  steps: [
+    {
+      name: 'Checkout',
+      uses: 'actions/checkout@v2',
+      with: {
+        ref: "${{ github.base_ref }}",
+        repository: "${{ github.event.pull_request.head.repo.full_name }}",
+      }
+    },
+    {
+      name: 'Setup cfn-lint',
+      uses: 'scottbrenner/cfn-lint-action@v2',
+    },
+    {
+      name: 'Install dependencies',
+      run: 'yarn install --check-files',
+    },
+    {
+      name: 'Build',
+      run: 'yarn build',
+    },
+    {
+      name: 'Save CloudFormation templates',
+      uses: 'actions/upload-artifact@v3',
+      with: {
+        name: 'base-branch-templates',
+        path: 'cdk.out'
+      },
+    },
+  ]
+});
+
+project.buildWorkflow.addPostBuildJob('cfn-diff', {
+  permissions: { contents: JobPermission.READ },
+  runsOn: ['ubuntu-latest'],
+  needs: ['build', 'build_target'],
+  steps: [
+    {
+      name: 'Download base branch CloudFomration templates',
+      uses: 'actions/download-artifact@v3',
+      with: {
+        name: 'base-branch-templates',
+        path: 'cdk.out.base'
+      },
+    },
+    {
+      name: 'Download build CloudFomration templates',
+      uses: 'actions/download-artifact@v3',
+      with: {
+        name: 'build-templates',
+        path: 'cdk.out.build'
+      },
+    },
+    {
+      name: 'Diff',
+      run: 'diff -r -q cdk.out.build cdk.out.base' // Maybe use cdk diff here?
+    },
+  ]
+})
+
+
+
 
 project.synth();
